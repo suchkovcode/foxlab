659df094a02f4ea7bdeed9b0d04a03585bba7cab
Nikita Suchkov <suchkov8520@gmail.com>
2022-08-12 14:16:39 +0300

    Add responsive section-4

---
+++ src/js/marquee.js
@@ -0,0 +1,16 @@
+$(".section-4__marquee-1").marquee({
+   duration: 22000,
+   gap: 20,
+   delayBeforeStart: 0,
+   startVisible: true, 
+   direction: 'left',
+   duplicated: true,
+});
+$(".section-4__marquee-2").marquee({
+   duration: 22000,
+   gap: 20,
+   delayBeforeStart: 0,
+   startVisible: true,
+   direction: 'right',
+   duplicated: true,
+});
