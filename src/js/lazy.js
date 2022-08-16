5493fed7cfb625a16b6bb5d100d87a1e824b7c29
Nikita Suchkov <suchkov8520@gmail.com>
2022-08-16 15:04:37 +0300

    Add lazy load video and fix style

---
+++ src/js/lazy.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+   let lazyLoadVideos = [].slice.call(document.querySelectorAll(".lazy-video"));
+   if ("IntersectionObserver" in window) {
+      let lazyVideoObserver = new IntersectionObserver(function (entries, observer) {
+         entries.forEach(function (video) {
+            if (video.isIntersecting) {
+               for (let source in video.target.children) {
+                  let videoSource = video.target.children[source];
+                  if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+                     videoSource.src = videoSource.dataset.src;
+                  }
+               }
+               video.target.load();
+               video.target.classList.remove("lazy-video");
+               lazyVideoObserver.unobserve(video.target);
+            }
+         });
+      });
+      lazyLoadVideos.forEach(function (lazyVideo) {
+         lazyVideoObserver.observe(lazyVideo);
+      });
+   }
+});
