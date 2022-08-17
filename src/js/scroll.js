c82450c266739513a64d65c5dc91f57385eb93e9
Nikita Suchkov <suchkov8520@gmail.com>
2022-08-17 14:31:10 +0300

    Add scoll script

---
+++ src/js/scroll.js
@@ -0,0 +1,10 @@
+SmoothScroll({
+   animationTime    : 800,
+   stepSize         : 75,
+   keyboardSupport   : true,  
+   arrowScroll       : 75,
+   pulseAlgorithm   : true,
+   pulseScale       : 4,
+   pulseNormalize   : 1,
+   touchpadSupport   : true,
+})
